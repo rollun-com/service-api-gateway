8109d7652784fa460de98646fdf5a7df00250c7f
victorynox <it.professor02@gmail.com>
2017-08-07 20:30:16 +0300

    Add config and requires lib

--- config/routes.php
+++ config/routes.php
@@ -28,27 +28,30 @@
 
 /*$app->get('/', App\Action\HomePageAction::class, 'home');*/
 
-if($container->has('home-service')){
-    $app->route('/','home-service' ,['GET'],'home-page');
+if ($container->has('home-service')) {
+    $app->route('/', 'home-service', ['GET'], 'home-page');
 }
-if($container->has('api-datastore')){
-    $app->route('/api/datastore[/{resourceName}[/{id}]]','api-datastore',['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],'api-datastore');
+if ($container->has('crud-service')) {
+    $app->route('/crud', 'crud-service', ['GET'], 'crud-page');
 }
-if($container->has('webhookActionRender')){
-    $app->route('/webhook[/{resourceName}]','webhookActionRender',['GET', 'POST'],'webhook');
+if ($container->has('api-datastore')) {
+    $app->route('/api/datastore[/{resourceName}[/{id}]]', 'api-datastore', ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], 'api-datastore');
 }
-if($container->has('loginPageAR')){
-    $app->route('/login','loginPageAR' ,['GET','POST'],'login-page');
+if ($container->has('webhookActionRender')) {
+    $app->route('/webhook[/{resourceName}]', 'webhookActionRender', ['GET', 'POST'], 'webhook');
 }
-if($container->has('loginServiceAR')){
-    $app->route('/login/{resourceName}','loginServiceAR' ,['GET','POST'],'login-service');
+if ($container->has('loginPageAR')) {
+    $app->route('/login', 'loginPageAR', ['GET', 'POST'], 'login-page');
 }
-if($container->has('loginPrepareServiceAR')){
-    $app->route('/login_prepare/{resourceName}','loginPrepareServiceAR' ,['GET','POST'],'login-prepare-service');
+if ($container->has('loginServiceAR')) {
+    $app->route('/login/{resourceName}', 'loginServiceAR', ['GET', 'POST'], 'login-service');
 }
-if($container->has('logoutAR')){
-    $app->route('/logout','logoutAR' ,['GET','POST'],'logout');
+if ($container->has('loginPrepareServiceAR')) {
+    $app->route('/login_prepare/{resourceName}', 'loginPrepareServiceAR', ['GET', 'POST'], 'login-prepare-service');
 }
-if($container->has('user-page')){
-    $app->route('/user','user-page' ,['GET','POST'],'user-page');
+if ($container->has('logoutAR')) {
+    $app->route('/logout', 'logoutAR', ['GET', 'POST'], 'logout');
+}
+if ($container->has('user-page')) {
+    $app->route('/user', 'user-page', ['GET', 'POST'], 'user-page');
 }
